f4f90f9797edc34667a9c9ed8b2481b6c1c01252
margo-lee <margo.lee@hyweb.com.tw>
2025-06-23 15:33:07 +0800

    feat:新增說明區域

--- src/views/admin/searchComponent.vue
+++ src/views/admin/searchComponent.vue
@@ -2,9 +2,38 @@
   <v-container class="container">
     <v-breadcrumbs :items="['首頁', '節點', '節點']"></v-breadcrumbs>
     <v-row>
-      <h2><span>補償酬金調整設定</span></h2>
+      <h2>
+        <span>補償酬金調整設定</span>
+      </h2>
+      <h2>
+        <span
+          >補償酬金調整設定
 
+          <v-btn size="sm" variant="text" @click="expand3 = !expand3">
+            <span class=""> {{ expand3 ? "收合說明" : "展開說明" }}</span>
+            <v-icon icon="mdi-chevron-down" size="x-small"></v-icon>
+          </v-btn>
+        </span>
+      </h2>
+      <v-col cols="12 py-0">
+        <v-expand-transition>
+          <v-card v-show="expand3" class="SearchInfoContent">
+            <ul>
+              <li>
+                消你各至問鴨唱動，真收都還消再方說才上乍支未風。馬草我抓的斥！蝸羽婆來眼白爪申；由松朵工訴干兔牠連加，四彩想晚西蝴！玩抄祖而司青弓千童少同；邊雨申讀牙肉河朱、誰六旁女住再借朵讀屋，友追加，故五做汗。
+              </li>
+              <li>
+                央收開打幼抄秋書彩，春苗候音讀黑旁遠香帶、文空可，愛千九巴羽飽昔息園登爪幫，原流兆視冒新枝兆路各。爸主今。
+              </li>
+              <li>
+                杯愛掃成九兌，品坡起左用讀「尼點果子哪笑公就」雨放坡實午圓河巴出止幫元孝胡夕，首成食也，教馬來又，黃里貓也從忍時走室斤五朱、做苦十遠。
+              </li>
+            </ul>
+          </v-card>
+        </v-expand-transition>
+      </v-col>
       <!-- 查詢區塊 start -->
+
       <v-col class="d-flex align-center" cols="12">
         <h3 class="mx-2 h3">
           查詢條件
@@ -194,6 +223,7 @@ export default {
     tab: null,
     expand: false,
     expand2: false,
+    expand3: false,
     functionGroupToggle: 0,
     page: 1,
     panel: 1,
